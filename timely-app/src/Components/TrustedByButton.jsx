d45f54db6ae8c55592b00f5460a8cc713aa9e3ea
Momin-Mohammad <mominmohammad111@gmail.com>
2022-11-17 18:33:54 +0530

    css changes

--- timely-app/src/Components/TrustedByButton.jsx
+++ timely-app/src/Components/TrustedByButton.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Image, Text } from "@chakra-ui/react";
+import { Box, Image, Text } from "@chakra-ui/react";
 import styles from "./TrustedByButton.module.css"
 
 export default function TrustedByButton(props){
